60e088e2994e522e7199707538f8bcd97ec91a62
magicboro <igor@borozd.in>
2018-02-04 21:07:32 +0300

    Add first page with JS

---
+++ js/animations.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("logo1").style.display = 'none';
+    document.getElementById("logo2").style.display = 'block';
+    document.getElementById("fio").style.display = 'block';
+    document.getElementById("contacts").style.display = 'block';
+    document.getElementById("website").style.background = '#263238';
+}
