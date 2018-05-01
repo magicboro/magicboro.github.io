b4875530174ce4be99a5c22d9d01ae98b3b6129c
magicboro <igor@borozd.in>
2018-05-01 23:44:12 +0300

    JS fixes for jquery

--- js/animations.js
+++ js/animations.js
@@ -1,24 +1,40 @@
-
-function myFunction() {
-    document.getElementById("logo1").style.display = 'none';
-    document.getElementById("logo2").style.display = 'block';
-    document.getElementById("fio").style.display = 'block';
-    document.getElementById("contacts").style.display = 'block';
-    document.getElementById("website").style.background = '#263238';
-}
-
-function myFunction1() {
-    document.getElementById("website").style.background = '#263238';
-}
-
-function mouseOver() {
-    document.getElementById("website").style.background = '#3949AB';
-    document.getElementById("website").style.transition = '400ms';
-}
-
-function mouseOut() {
-    document.getElementById("website").style.background = '#263238';
-}
-
-setTimeout(myFunction, 3500);
-setTimeout(myFunction1, 2000);
+$(document).ready(function(){
+    $("#logo1").fadeIn(2000);
+    setTimeout(function(){
+      $("#logo1").hide(0, function(){
+          $("#logo2").show();
+          $("#fio").show();
+          $("#contacts").show();
+          $("#website").css("background", "#263238");
+        });
+      }, 2000);
+    $('#website').click(function(){
+      $('#logo1').hide(0, function(){
+          $("#logo2").show();
+          $("#fio").show();
+          $("#contacts").show();
+          $("#website").css("background", "#263238");
+        });
+    });
+    $("#logo").on({
+      mouseenter: function(){
+        $("#website").css({
+          "background": "#3949AB",
+          "transition": "400ms"
+        });
+        $(this).css({
+          'transform': 'rotate(-90deg)',
+          'margin-left': '-20px'
+        });
+      },
+      mouseleave: function(){
+        $("#website").css({
+          "background": "#263238"
+        });
+        $(this).css({
+          'transform': 'rotate(0deg)',
+          'margin-left': '0px'
+        });
+      }
+    });
+});
